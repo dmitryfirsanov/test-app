44918be22fb2b7b5a0bef698c016f2e51c5e65e9
Dmitry Firsanov <dmirtyfirsanov03@gmail.com>
2024-09-26 21:44:46 +0300

    add assets, initial styles and etc.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@import "@/assets/scss/_vars.scss"; @import "@/assets/scss/_typography.scss";',
+        },
+      },
+    },
+  },
+  css: ['normalize.css/normalize.css', '@/assets/scss/style.scss']
 })
